e649692b263d49959b4eab8bf973ec441fe3373d
RJ Robinson <Robert_Robinson3@cable.comcast.com>
2015-12-31 06:50:30 -0500

    Initial Commit

---
+++ auto_layout_constraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auto_layout_constraints
+//
+//  Created by Robinson, Robert on 12/31/15.
+//  Copyright © 2015 Robinson, Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ auto_layout_constraintsTests/auto_layout_constraintsTests.swift
@@ -0,0 +1,36 @@
+//
+//  auto_layout_constraintsTests.swift
+//  auto_layout_constraintsTests
+//
+//  Created by Robinson, Robert on 12/31/15.
+//  Copyright © 2015 Robinson, Robert. All rights reserved.
+//
+
+import XCTest
+@testable import auto_layout_constraints
+
+class auto_layout_constraintsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
